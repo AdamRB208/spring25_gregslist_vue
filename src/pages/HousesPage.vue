c83a3af271d52a09dce179af63efd985776a1a74
AdamRB208 <adamrborup@gmail.com>
2025-03-19 14:21:46 -0600

    created house service and get function

--- src/pages/HousesPage.vue
+++ src/pages/HousesPage.vue
@@ -1,4 +1,23 @@
 <script setup>
+import { houseService } from '@/services/HousesService.js';
+import { logger } from '@/utils/Logger.js';
+import { Pop } from '@/utils/Pop.js';
+import { onMounted } from 'vue';
+
+
+
+onMounted(() => {
+  getHouses()
+})
+
+async function getHouses() {
+  try {
+    await houseService.getHouses()
+  } catch (error) {
+    Pop.error(error, 'Could not get houses')
+    logger.error('COULD NOT GET HOUSES', error)
+  }
+}
 
 </script>
 

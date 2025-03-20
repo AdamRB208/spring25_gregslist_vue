724f46b0e749c0394587120116dbafc3806c2910
AdamRB208 <adamrborup@gmail.com>
2025-03-20 00:35:48 -0600

    added jobs form

---
+++ src/components/JobForm.vue
@@ -0,0 +1,52 @@
+<script setup>
+
+</script>
+
+
+<template>
+  <section class="contioner">
+    <div class="row">
+      <div class="col-6">
+        <form onsubmit="app.jobController.createJobListing()">
+          <div class="mb-3">
+            <label for="company">Company</label>
+            <input id="company" name="company" type="text" required min="1" max="30" placeholder="Company Name Here...">
+          </div>
+          <div class="mb-3">
+            <label for="jobTitle">Job Title</label>
+            <input id="jobTitle" name="jobTitle" type="text" required min="1" max="30" placeholder="Job Title Here...">
+          </div>
+          <div class="mb-3">
+            <label for="hours">Hours</label>
+            <input id="hours" name="hours" type="number" required min="1" max="24" placeholder="0">
+          </div>
+          <div class="mb-3">
+            <label for="rate">Rate</label>
+            <input id="rate" name="rate" type="number" required min="1" max="1000000" placeholder="$0.00">
+          </div>
+          <div class="mb-3">
+            <label for="jobDescription">Job Description</label>
+            <textarea name="jobDescription" id="jobDescription" type="text" class="w-100"
+              placeholder="Description of the job..." maxlength="500"></textarea>
+          </div>
+          <!-- <div class="mb-3">
+    <label for="creatorId">creatorId</label>
+    <input id="creatorId" name="creatorId" type="">
+  </div>
+  <div class="mb-3">
+    <label for="id">id</label>
+    <input id="id" name="id" type="" >
+  </div> -->
+          <div class="text-end">
+            <button class="btn btn-outline-success" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </section>
+</template>
+
+
+<style lang="scss" scoped></style>
--- src/pages/JobsPage.vue
+++ src/pages/JobsPage.vue
@@ -5,6 +5,7 @@ import { logger } from '@/utils/Logger.js';
 import { Pop } from '@/utils/Pop.js';
 import { computed, onMounted } from 'vue';
 import JobListing from '@/components/JobListing.vue';
+import JobForm from '@/components/JobForm.vue';
 
 const jobs = computed(() => AppState.jobs)
 
@@ -29,6 +30,20 @@ async function getJobs() {
       <div class="col-12">
         <div class="text-center">
           <h1 class="display-3">Jobs</h1>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="container">
+    <div class="row align-items-center">
+      <div class="col-md-6">
+        <JobForm />
+      </div>
+      <div class="col-md-6">
+        <div class="text-center my-3">
+          <img
+            src="https://images.unsplash.com/photo-1601342630314-8427c38bf5e6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8d29ya2luZ3xlbnwwfDF8MHx8fDI%3D"
+            alt="image of hands working" class="img-fluid rounded-5 shadow-lg">
         </div>
       </div>
     </div>

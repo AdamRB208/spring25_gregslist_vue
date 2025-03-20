266a442501607736fc6d8a48b07f70c5ee61c389
AdamRB208 <adamrborup@gmail.com>
2025-03-19 23:07:10 -0600

    Got Jobs!

--- src/pages/JobsPage.vue
+++ src/pages/JobsPage.vue
@@ -1,5 +1,22 @@
 <script setup>
+import { jobsService } from '@/services/JobsService.js';
+import { logger } from '@/utils/Logger.js';
+import { Pop } from '@/utils/Pop.js';
+import { onMounted } from 'vue';
 
+
+onMounted(() => {
+  getJobs()
+})
+
+async function getJobs() {
+  try {
+    await jobsService.getJobs()
+  } catch (error) {
+    Pop.error(error, 'Could not get Jobs')
+    logger.error('COULD NOT GET JOBS', error)
+  }
+}
 </script>
 
 
